108b4dac6bd4caff2a1e52404ae87140b397695d
v-yinyangyang <v-yinyangyang@saicmobility.com>
2019-10-11 19:48:55 +0800

    不可变数据机构

--- react/jsx/demo-1.js
+++ react/jsx/demo-1.js
@@ -25,3 +25,52 @@ function App2(){
 }
 
 // 2019.10.10
+
+// 根据props来渲染不同组件
+const components = {
+    photo: PhotoStory,
+    video: VideoStory
+}
+
+function Story(props){
+    const SpecificStory = components[props.storyType]
+    return <SpecificStory story={props.story} />
+}
+
+
+const Button = props => {
+    const {kind, ...other} = props
+    const className = kind === "primary" ? "PrimaryButton" : "SecondaryButton"
+    return <button className={className} {...other} />
+}
+
+const App = () => {
+    return (
+        <div>
+            <Button kind="primary" onClick={() => console.log("clicked!")}>
+                Hello World!
+            </Button>
+        </div>
+    )
+}
+
+
+//性能优化
+
+// Chrome Performance 标签来分析组件
+
+// concat()不会更改现有数组，返回一个型数组
+
+function updateColorMap(colormap) {
+    colormap.right = 'blue'
+}
+
+function updateColorMap(colormap){
+    return Object.assign({},colormap,{right:'blue'})
+}
+
+function updateColorMap(colormap){
+    return {...colormap,right:'blue'}
+}
+
+// immutable.js  不可变数据结构
